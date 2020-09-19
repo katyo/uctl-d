59874f1b8b586006c7e2d619524321080546cb58
K <kayo@illumium.org>
2020-09-19 12:25:06 +0500

    Public export filters

--- uctl/filt/package.d
+++ uctl/filt/package.d
@@ -5,11 +5,11 @@
 */
 module uctl.filt;
 
-import uctl.filt.avg;
-import uctl.filt.ema;
-import uctl.filt.fir;
-import uctl.filt.lqe;
-import uctl.filt.med;
+public import uctl.filt.avg: avg;
+public import uctl.filt.med: med;
+public import uctl.filt.ema: EmaParam = Param, EmaState = State, EmaAlpha = Alpha, EmaSamples = Samples, EmaTime = Time, EmaPT1 = PT1;
+public import uctl.filt.fir: FirParam = Param, FirState = State;
+public import uctl.filt.lqe: LqeParam = Param, LqeState = State;
 
 alias uctl.filt.ema.mk mk;
 alias uctl.filt.fir.mk mk;
